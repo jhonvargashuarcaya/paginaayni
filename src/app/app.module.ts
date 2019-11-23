6a6407d7eaa629b8aef9ba1a8322ab29f56365a9
jhonvargashuarcaya <jvargashuarcaya@gmail.com>
2019-11-23 12:14:14 -0500

    navar90%

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavebarComponent } from './navebar/navebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavebarComponent
   ],
   imports: [
     BrowserModule,
