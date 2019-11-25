823c686baf4cf38e9378dfaacff977d2c8b69360
millermax <romel_23_3@hotmail.com>
2019-11-25 15:37:45 -0500

    componentes creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavebarComponent } from './navebar/navebar.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { ProyectosComponent } from './proyectos/proyectos.component';
+import { MultimediaComponent } from './multimedia/multimedia.component';
+import { ContactenosComponent } from './contactenos/contactenos.component';
+import { FinalComponent } from './final/final.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavebarComponent
+    NavebarComponent,
+    ServiciosComponent,
+    ProyectosComponent,
+    MultimediaComponent,
+    ContactenosComponent,
+    FinalComponent
   ],
   imports: [
     BrowserModule,
